44ea2ca6ee7824de8a22bb6fd26d941973096a49
Loïc DURAND <loicdurand.ld@gmail.com>
2025-03-31 21:36:11 -0400

    Affichage du menu contextuel (Card) fonctionnel

--- src/UI/Card.vue
+++ src/UI/Card.vue
@@ -52,9 +52,19 @@ export default {
       navigator.clipboard.writeText(content);
       fadeOut(target);
     },
-    show_menu(e: Event): void {
-      const target = e.currentTarget as HTMLElement;
+    toggle_menu(e: Event): void {
+      const target = e.target as HTMLElement;
       const menu_id = target?.dataset.id;
+      const menu = document.getElementById(`nav-${menu_id}`);
+      const others = document.querySelectorAll(`.card-menu:not(#nav-${menu_id})`);
+
+      console.log(menu_id);
+      [...others].forEach(elt => {
+        elt.classList.remove("active");
+      });
+      if (menu) {
+        menu.classList.toggle('active');
+      }
     }
   }
 }
@@ -117,13 +127,80 @@ export default {
           class="material-icons"
           title="Afficher le menu"
           :data-id="id"
-          @click="show_menu"
+          @click="toggle_menu"
         >more_horiz</i>
       </span>
 
-    </footer>
+      <nav
+        :id="'nav-' + id"
+        class="card-menu"
+      >
+        <ul>
+          <li>
+            <input
+              type="radio"
+              :id="'details-option-' + id"
+              name="selector"
+            >
+            <label :for="'details-option-' + id">Détails</label>
 
-    <nav :id="'nav-'+id" class="card-menu"></nav>
+            <div class="check">
+              <div class="inside"></div>
+            </div>
+          </li>
+          <li>
+            <input
+              type="radio"
+              :id="'dl-option-' + id"
+              name="selector"
+            >
+            <label :for="'dl-option-' + id">Télécharger</label>
+
+            <div class="check">
+              <div class="inside"></div>
+            </div>
+          </li>
+          <li>
+            <input
+              type="radio"
+              :id="'share-option-' + id"
+              name="selector"
+            >
+            <label :for="'share-option-' + id">Partager</label>
+
+            <div class="check">
+              <div class="inside"></div>
+            </div>
+          </li>
+          <li>
+            <input
+              type="radio"
+              :id="'mod-option-' + id"
+              name="selector"
+            >
+            <label :for="'mod-option-' + id">Modifier</label>
+
+            <div class="check">
+              <div class="inside"></div>
+            </div>
+          </li>
+          <li>
+            <input
+              type="radio"
+              :id="'suppr-option-' + id"
+              name="selector"
+            >
+            <label :for="'suppr-option-' + id">Supprimer</label>
+
+            <div class="check">
+              <div class="inside"></div>
+            </div>
+          </li>
+        </ul>
+
+      </nav>
+
+    </footer>
 
   </article>
 
@@ -216,12 +293,111 @@ export default {
       width: 4vh;
       right: 1rem;
     }
-  }
 
-  & .card-menu{
-    width:10px;
-    height: 10px;
-    background-color: red;
+    // menu
+    & .card-menu {
+      width: 100%;
+      max-height: 0;
+      background-color: var(--grey-0);
+      position: absolute;
+      bottom: 0;
+      border-top-left-radius: 14px;
+      border-top-right-radius: 14px;
+      overflow-y: scroll;
+      transition: max-height .25s ease;
+      -webkit-transition: max-height .25s ease;
+
+      &.active {
+        height: 46vh;
+        max-height: 46vh;
+      }
+
+      & ul {
+        list-style: none;
+        margin: 60px 0 0 0;
+        padding: 0;
+        overflow: auto;
+        border-top: 1px solid var(--grey-3);
+      }
+
+      & ul li {
+        color: #AAAAAA;
+        display: block;
+        position: relative;
+        float: left;
+        width: 100%;
+        height: 60px;
+        border-bottom: 1px solid var(--grey-3);
+      }
+
+      & ul li input[type=radio] {
+        position: absolute;
+        visibility: hidden;
+      }
+
+      & ul li label {
+        display: block;
+        position: relative;
+        font-weight: 300;
+        font-size: 18px;
+        padding: 7px;
+        margin: 10px auto;
+        height: 30px;
+        z-index: 9;
+        cursor: pointer;
+        -webkit-transition: all 0.25s linear;
+      }
+
+      & ul li:hover label {
+        color: var(--grey-7);
+      }
+
+      & ul li .check {
+        display: block;
+        position: absolute;
+        border: 4px solid #AAAAAA;
+        border-radius: 100%;
+        height: 25px;
+        width: 25px;
+        top: 18px;
+        left: 20px;
+        z-index: 5;
+        transition: border .25s linear;
+        -webkit-transition: border .25s linear;
+      }
+
+      & ul li:hover .check {
+        border: 4px solid var(--grey-7);
+      }
+
+      & ul li .check::before {
+        display: block;
+        position: absolute;
+        content: "";
+        border-radius: 100%;
+        height: 7px;
+        width: 7px;
+        top: 5px;
+        left: 5px;
+        margin: auto;
+        transition: background 0.25s linear;
+        -webkit-transition: background 0.25s linear;
+      }
+
+      & input[type=radio]:checked~.check {
+        border: 4px solid var(--theme-color);
+      }
+
+      & input[type=radio]:checked~.check::before {
+        background: var(--theme-color);
+      }
+
+      & input[type=radio]:checked~label {
+        color: var(--theme-color);
+      }
+    }
+
+    // fin menu
   }
 
   /* fin footer */
